a6705d99961ea79e9619dde661c7258346ab784b
kartiksingh428 <kartik.singh@beritrave.com>
2026-04-06 10:52:37 +0530

    url

--- src/lib/api.js
+++ src/lib/api.js
@@ -1,5 +1,5 @@
 const isDev = import.meta.env.MODE === "development";
 
 export const API_URL = isDev
-  ? "https://blog-backend-j816.onrender.com/api/v1"      // development
+  ? "http://localhost:3000/api/v1"      // development
   : "https://blog-backend-j816.onrender.com/api/v1";     // production
